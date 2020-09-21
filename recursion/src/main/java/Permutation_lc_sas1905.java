dd2149f6048b2af241d42007752ee68a9bccd3f4
Sasmit Chatterjee <sasmitchatterjee.19@gmail.com>
2020-09-22 03:16:17 +0530

    All Permutations of Distinct list.

---
+++ recursion/src/main/java/Permutation_lc_sas1905.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a collection of distinct integers, return all possible permutations.
+ *
+ * Example:
+ *
+ * Input: [1,2,3]
+ * Output:
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1],
+ *   [3,1,2],
+ *   [3,2,1]
+ * ]
+ */
+public class Permutation_lc_sas1905 {
+      static int N=0;
+      static List<Integer> given_list = new ArrayList<>();
+
+    public static void main(String[] args) {
+         List<Integer> given = new ArrayList<>();
+         given.add(1);
+         given.add(2);
+         given.add(3);
+         given.add(4);
+         given.add(5);
+         solve_ques(given);
+    }
+
+    private static void solve_ques(List<Integer> given) {
+        N = given.size();
+        given_list = given;
+        permute_with_recursion(given_list,0,N-1);
+    }
+
+    private static void permute_with_recursion(List<Integer> given_list, int lo, int hi) {
+        if(lo == hi){
+            given_list.forEach((Integer value) -> System.out.print(value+""));
+            System.out.println();
+            return;
+        }
+        for(int i=lo;i<=hi;i++){
+            Collections.swap(given_list,lo,i);
+            permute_with_recursion(given_list,lo+1,hi);
+            Collections.swap(given_list,lo,i);
+        }
+        return;
+    }
+}
