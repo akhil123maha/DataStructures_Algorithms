799f33542dfb6400f316a8b9142670982352435d
Akhil Mahajan <akhil123maha@gmail.com>
2020-10-07 19:17:07 +0530

    Facebook Prep

--- recursion/src/main/java/com/ds/algo/facebook/leetcode/WordDictionary.java
+++ recursion/src/main/java/com/ds/algo/facebook/leetcode/WordDictionary.java
@@ -2,11 +2,10 @@ package com.ds.algo.facebook.leetcode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class WordDictionary {
 
-    public class TrieNode{
+    private class TrieNode{
         Map<Character, TrieNode> children;
         boolean isEndOfWord;
 
@@ -18,12 +17,12 @@ public class WordDictionary {
     private TrieNode root;
 
     /** Initialize your data structure here. */
-    public WordDictionary() {
+    private WordDictionary() {
         root = new TrieNode();
     }
 
     /** Adds a word into the data structure. */
-    public void addWord(String word) {
+    private void addWord(String word) {
         TrieNode current = root;
         int length = word.length();
         for (int i = 0; i < length; i++) {
@@ -41,23 +40,47 @@ public class WordDictionary {
     }
 
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-    public boolean search(String word) {
-        TrieNode current = root;
-        int length = word.length();
-        for (int i = 0; i < length; i++) {
-            char character = word.charAt(i);
-            if(character == '.'){
-                Set<Character> characters = current.children.keySet();
+    private boolean search(String word) {
+       return search(word, root);
+    }
 
-            } else {
-                TrieNode temp = current.children.get(character);
-                if(temp == null){
-                    return false;
-                }
-                current = temp;
+    private boolean search(String word, TrieNode node) {
+        if (word == null || (word.isEmpty())) {
+            return node.isEndOfWord;
+        }
+        char c = word.charAt(0);
+        if (c != '.') {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            else {
+                return search(word.substring(1), node.children.get(c));
             }
         }
-        return current.isEndOfWord;
+        else {
+            // Collection<TrieNode> children = node.children.values();
+            for (TrieNode child : node.children.values()) {
+                if (search(word.substring(1), child)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        boolean pad = wordDictionary.search("pad");// return False
+        System.out.println("pad = " + pad);
+        boolean bad = wordDictionary.search("bad");// return True
+        System.out.println("bad = " + bad);
+        boolean search = wordDictionary.search(".ad");// return True
+        System.out.println("search = " + search);
+        boolean search1 = wordDictionary.search("b..");// return True
+        System.out.println("search1 = " + search1);
     }
 
 }
